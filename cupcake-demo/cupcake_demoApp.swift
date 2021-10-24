2d2a4fe1b015a2cc7b30edecb57419ccd51eee38
ganudoomer <ganuiscool@gmail.com>
2021-10-24 12:58:41 +0530

    Initial Commit

---
+++ cupcake-demo/cupcake_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cupcake_demoApp.swift
+//  cupcake-demo
+//
+//  Created by Sree on 24/10/21.
+//
+
+import SwiftUI
+
+@main
+struct cupcake_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
